767d14ccc41eff760f461e1bff31d0a393b97c06
Evgenii <krasnoperov007@mail.ru>
2023-04-17 00:35:27 -0400

    Backtest() refactored

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -1,132 +1,113 @@
-from multiprocessing import Pool
 import pandas as pd
-from main import TradingBot
-from strategies.channel_slope import ChannelSlope
+
+from brokers.binance import Binance
+from analytics.performanceAnalytics import TradeAnalysis
+from playground.positionManagement import PositionManagement
+from strategies.channelSlope import ChannelSlope
+from strategies.vamo import Vamo
+from utils.utils import time_it
 
 '''
-    obj - strategy class with df = "Strategy(dataframe)" that has the following:
+    strategy - strategy class with df = "Strategy(dataframe)" that has the following:
     set_random_vals() - set-up random values for Monte-Carlo
-    num_runs - number of iterations
 '''
 
 
 class Backtest:
 
-    def __init__(self, obj):
-        self.obj = obj  # <= strategy class
-        self.num_runs = 1
-        self.montecarlo = False
-        self.position = None
-        self.total_profit = 0
-        self.num_positions = 4  # maximum number of positions that can be open simultaneously
-        self.positions = []  # list of currently open positions
+    def __init__(self, strategy):
+        self.strategy = strategy  # <= strategy class
+        self.PM = PositionManagement(strategy)
+        self.statistics_dataframe = pd.DataFrame(
+            columns=['Timestamp', 'Signal', 'Price', 'Amount', 'Close Profit', 'Total Profit'])
 
-    def apply_strategy(self, destination_df):
-        try:
-            signal = self.obj.run(destination_df)
-            current_price = destination_df['close'].iloc[-1]
-            if signal == 'BUY__':
-                if self.position is None and len(self.positions) < self.num_positions:
-                    self.positions.append(current_price)
-                    print('BUY:', destination_df['timestamp'].iloc[-1], 'price:', current_price, self.positions)
-            elif signal == 'CLOSE':
-                if self.position is not None or len(self.positions) > 0:
-                    for position_price in self.positions:
-                        profit = current_price - position_price
-                        self.total_profit += profit
-                    self.positions = []
-                    print('CLOSE:', destination_df['timestamp'].iloc[-1], 'price:', current_price, 'total profit:', self.total_profit)
-        except:
-            pass
-
-    '''
-    static backtest is for existing static dataframe only, for live data will use live backtest  
-    '''
-
-    def apply_strategy_old(self, destination_df):
-        try:
-            signal = self.obj.run(destination_df)
-            if signal in {'BUY__', 'CLOSE'}:
-                print(signal, destination_df['timestamp'].iloc[-1], 'price:', destination_df['close'].iloc[-1])
-        except:
-            pass
-
-    def run_static_simulation(self, n):
-        n = n  # the number of runs
-        if self.montecarlo: self.obj.set_random_vals()
-        try:
-            """backtest itself"""
-            self.obj.run_test()
-            """
-            '''reporting'''
-            run_params = vars(self.obj)
-            run_params.pop('dataframe')
-            #print(run_params)
-            """
-        except:
-            pass
+    def set_custom_params(self, params):
+        self.PM.max_amount, \
+        self.PM.max_positions, \
+        self.PM.stop_loss, \
+        self.PM.take_profit, \
+        self.PM.lot_one, \
+        self.PM.lot_two, \
+        self.PM.lot_three = params
 
     """iterate thru dataframe and add the last row of initial dataframe to a new one
     while applying all the indicators and identifying entry points"""
 
-    def run_live_simulation(self, n=None):
-        n = self.num_runs  # the number of runs
-        source_df = self.obj.obj.broker.dataframe
+    def run_live_simulation(self):
+        source_df = self.strategy.dataframe
         destination_df = pd.DataFrame(columns=source_df.columns)
-        max_rows = 50
-        if self.montecarlo:
-            self.obj.set_random_vals()
+        max_rows = 60
         # loop through each row in the source DataFrame and transfer it to the destination DataFrame
         for i, row in source_df.iterrows():
             # append the row to the destination DataFrame
             destination_df = pd.concat([destination_df, row.to_frame().T], ignore_index=True)
             # check if the number of rows in destination_df exceeds max_rows
             if len(destination_df) > max_rows:
-                # drop the oldest row
                 destination_df = destination_df.iloc[1:]
-            # apply strategy to see if there is a signal
-            self.apply_strategy(destination_df)
-        print('profit:', self.total_profit)
-        return -self.total_profit
+            if len(destination_df) == max_rows:
+                self.PM.apply_strategy(destination_df)
+                self.statistics_dataframe = self.PM.statistics_dataframe
+        # return total profit for optimize.minimize
+        return self.PM.total_profit
+
+    def run_static_simulation(self):
+        try:
+            """backtest itself"""
+            self.strategy.run_test()
+        except:
+            pass
+
+    def live_simulation(self):
+        self.run_live_simulation()
+
+    def static_simulation(self):
+        self.run_static_simulation()
 
     def run_backtest(self, method=None):
         if method not in ['static_simulation', 'live_simulation']:
             raise ValueError("Invalid method specified. Use 'static_simulation' or 'live_simulation'.")
-        with Pool(5) as pool:
-            getattr(self, method)(pool, range(self.num_runs))
-
-    def static_simulation(self, pool, runs):
-        pool.map(self.run_static_simulation, runs)
-
-    def live_simulation(self, pool, runs):
-        pool.map(self.run_live_simulation, runs)
+        else:
+            getattr(self, method)()
 
 
 ''' obj - is DataStream object (eth, shib, etc.)'''
 
 
-def test_strategy(obj, simulation_type='live', strategy=ChannelSlope):
+def backtest_strategy(obj, simulation_type='live', strategy=ChannelSlope):
     # Pass the DataStream obj to the strategy class
     apply_strategy = strategy(obj)
-    #apply_strategy.set_custom_vals()
-    opt = [32, -23, 0.76, 0.62, 23, 21, 29]
-    apply_strategy.set_custom_vals_opt(opt)
-
-
+    apply_strategy.set_custom_vals()
     # Pass strategy to backtest
     apply_backtest = Backtest(apply_strategy)
-    apply_backtest.montecarlo = False
-    apply_backtest.num_runs = 1
+    vals = [15000, 1, 0.985, 1.1, 5, 5, 5]
+    apply_backtest.set_custom_params(vals)
 
     # Run the backtest with the appropriate method
-    apply_backtest.run_backtest(f"{simulation_type}_simulation")
+    apply_backtest.run_live_simulation()
+    # Run the analytics
+    performance = apply_backtest.statistics_dataframe
+    analysis = TradeAnalysis(performance)
+
+    analysis.modify_df()
+    print(analysis.df.to_string())
+    print(analysis.report())
+
+
+@time_it
+def main():
+    SYMBOL = 'ETHUSDT'
+    LIMIT = '100'
+    TIMEFRAME = '30m'
+    # time YYYY,M,D
+
+    START_TIME = '2023-4-5'
+    END_TIME = '2023-4-15'  # optional
+
+    eth_test = Binance(SYMBOL, TIMEFRAME, LIMIT, START_TIME, END_TIME)
+    backtest_strategy(eth_test, 'live')
+
+    return
 
 
 if __name__ == '__main__':
-
-    SYMBOL = 'LTCUSDT'
-    LIMIT = '150'
-    TIMEFRAME = '5m'
-
-    eth_test = TradingBot(SYMBOL, TIMEFRAME, LIMIT)
-    test_strategy(eth_test, 'live')
+    main()
