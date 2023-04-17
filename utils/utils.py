565e70e61e35c8fd2ca87525fe975485136e187e
Evgenii <krasnoperov007@mail.ru>
2023-04-17 00:41:27 -0400

    decorator to capture function's execution time added

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,16 @@
 from pathlib import Path
+from time import time
 
 
 def get_project_root() -> Path:
     return Path(__file__).parent.parent
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        result = func(*args, **kwargs)
+        end_time = time()
+        print(f"{func.__name__} took {end_time - start_time} seconds to execute.")
+        return result
+    return wrapper
